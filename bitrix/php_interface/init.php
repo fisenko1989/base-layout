a7838ff365a34ac7f88b43c049cdd9d9b9c2fc93
Roman Karpenko <neomoto@ya.ru>
2013-04-02 20:27:02 +0400

    added phpless

--- bitrix/php_interface/init.php
+++ bitrix/php_interface/init.php
@@ -3,3 +3,33 @@ require_once(dirname(__FILE__)."/include/functions.php");
 require_once(dirname(__FILE__)."/include/eventHandlers.php");
 require_once(dirname(__FILE__)."/include/customIblockProps.php");
 require_once(dirname(__FILE__)."/include/customUserProps.php");
+require_once(dirname(__FILE__)."/include/lessc.inc.php");
+
+define("LESS_PATH", $_SERVER['DOCUMENT_ROOT'].SITE_TEMPLATE_PATH."/less/styles.less");
+define("LESS_COMPILED_PATH", $_SERVER['DOCUMENT_ROOT'].SITE_TEMPLATE_PATH."/template_styles.css");
+
+function autoCompileLess($inputFile, $outputFile) {
+    
+    if(!file_exists($inputFile)){
+        return false;
+    }
+    
+    $cacheFile = $inputFile.".cache";
+
+    if (file_exists($cacheFile)) {
+        $cache = unserialize(file_get_contents($cacheFile));
+    } 
+    else {
+        $cache = $inputFile;
+    }
+
+    $less = new lessc();
+    $newCache = $less->cachedCompile($cache);
+
+    if (!is_array($cache) || $newCache["updated"] > $cache["updated"]) {
+        file_put_contents($cacheFile, serialize($newCache));
+        file_put_contents($outputFile, $newCache['compiled']);
+    }
+}
+
+autoCompileLess(LESS_PATH, LESS_COMPILED_PATH);
